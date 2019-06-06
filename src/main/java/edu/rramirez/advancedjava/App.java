313b63b76a017a1c647fa7c8d11bf38390af5afc
Ramon Ramirez <rramirez037@gmail.com>
2019-06-06 00:19:03 -0400

    initial commit

---
+++ src/main/java/edu/rramirez/advancedjava/App.java
@@ -0,0 +1,13 @@
+package edu.rramirez.advancejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
