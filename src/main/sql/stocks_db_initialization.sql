54af717b53cf76d68914fac38dd6d5f1511cafde
Ramon Ramirez <rramirez037@gmail.com>
2019-06-30 17:45:44 -0400

    submitting final changes

--- src/main/sql/stocks_db_initialization.sql
+++ src/main/sql/stocks_db_initialization.sql
@@ -3,6 +3,16 @@
 DROP TABLE IF EXISTS stocks.person_stock CASCADE;
 DROP TABLE IF EXISTS stocks.company CASCADE;
 DROP TABLE IF EXISTS stocks.person CASCADE;
+DROP TABLE IF EXISTS stocks.quotes CASCADE;
+
+CREATE TABLE stocks.quotes
+(
+   id INT NOT NULL AUTO_INCREMENT,
+   symbol VARCHAR(4) NOT NULL,
+   time DATETIME NOT NULL,
+   price DECIMAL NOT NULL,
+   PRIMARY KEY ( id )
+);
 
 
 /* creates a table to store a list of stock companies */
@@ -32,6 +42,11 @@ CREATE TABLE stocks.person_stock
 );
 
 /** now add some sample data */
+
+INSERT INTO stocks.quotes (symbol,time,price) VALUES ('GOOG','2004-08-19 00:00:01','85.00');
+INSERT INTO stocks.quotes (symbol,time,price) VALUES ('GOOG','2015-02-03 00:00:01','527.35');
+INSERT INTO stocks.quotes (symbol,time,price) VALUES ('APPL','2000-01-01 00:00:01','118.27');
+INSERT INTO stocks.quotes (symbol,time,price) VALUES ('AMZN','2015-02-03 00:00:01','363.21');
 
 INSERT INTO stocks.company (symbol,company_name) VALUES ('AAPL', 'APPLE INC');
 INSERT INTO stocks.company (symbol,company_name) VALUES ('GOOG', 'GOOGLE INC');
